926b2f1a7266e13972de827a6e59a28fd3086361
BaadAiim <dlfuaud1211@gmail.com>
2023-03-27 13:45:44 +0900

    17

--- 연습용/app.js
+++ 연습용/app.js
@@ -1,9 +1,20 @@
 const link = document.querySelector("a");
+const link2 = document.querySelector("b");
+const loginForm = document.querySelector("#폼");
+const loginForm2 = document.querySelector("#폼2");
 
 function handle1() {
     alert("Go To Git Hub");
 }
 
+function handle2() {
+    alert("Go To JS Crome");
+}
 
+function handle3(event) {
+    event.preventDefault();
+}
 
 link.addEventListener("click", handle1);
+link2.addEventListener("click", handle2);
+loginForm.addEventListener("submit", handle3);
