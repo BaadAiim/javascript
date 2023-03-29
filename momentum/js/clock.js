2327f8246cb5a279e6d20c480be2a275e80fb7f5
BaadAiim <dlfuaud1211@gmail.com>
2023-03-30 08:36:15 +0900

    21

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -2,7 +2,7 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
     const Date = new Date();
-    console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+    clock.innerText = console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
 }
 
 getClock()
