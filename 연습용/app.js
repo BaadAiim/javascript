71f69110e28de2223c5e14202f5919cd9ab489a7
BaadAiim <dlfuaud1211@gmail.com>
2023-04-01 22:38:46 +0900

    23

--- 연습용/app.js
+++ 연습용/app.js
@@ -1,6 +1,18 @@
 const link = document.querySelector("a");
 const link2 = document.querySelector("b");
 const loginForm = document.querySelector("#폼");
+const clock = document.querySelector("#h1");
+
+function getClock() {
+    const date = new Date()
+    const hours = String(date.getHours()).padStart("2", 0);
+    const minutes = String(date.getMinutes()).padStart("2", 0);
+    const seconds = String(date.getSeconds()).padStart("2", 0);
+    clock.innerText = `${hours}:${minutes}:${seconds}`; 
+}
+
+getClock()
+setInterval(getClock, 1000);
 
 function handle1() {
     alert("Go To Git Hub");
@@ -13,6 +25,8 @@ function handle2() {
 function handle3(event) {
     event.preventDefault();
 }
+
+
 
 link.addEventListener("click", handle1);
 link2.addEventListener("click", handle2);
