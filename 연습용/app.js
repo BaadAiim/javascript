2e502cfcf5e134e9ae8dce778f84142798185ae3
BaadAiim <dlfuaud1211@gmail.com>
2023-04-02 19:24:39 +0900

    24

--- 연습용/app.js
+++ 연습용/app.js
@@ -1,33 +1,12 @@
-const link = document.querySelector("a");
-const link2 = document.querySelector("b");
-const loginForm = document.querySelector("#폼");
-const clock = document.querySelector("#h1");
+const clock = document.querySelector("#clock");
 
 function getClock() {
-    const date = new Date()
-    const hours = String(date.getHours()).padStart("2", 0);
-    const minutes = String(date.getMinutes()).padStart("2", 0);
-    const seconds = String(date.getSeconds()).padStart("2", 0);
-    clock.innerText = `${hours}:${minutes}:${seconds}`; 
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock()
 setInterval(getClock, 1000);
-
-function handle1() {
-    alert("Go To Git Hub");
-}
-
-function handle2() {
-    alert("Go To JS Crome");
-}
-
-function handle3(event) {
-    event.preventDefault();
-}
-
-
-
-link.addEventListener("click", handle1);
-link2.addEventListener("click", handle2);
-loginForm.addEventListener("submit", handle3);
